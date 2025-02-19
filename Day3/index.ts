4ac91f14ab1b5c668c059d99912dc6801df2a736
atul gathwala <atulgathwala1728@gmail.com>
2025-02-19 20:55:43 +0530

    Day3

---
+++ Day3/index.ts
@@ -0,0 +1,80 @@
+// console.log("This is Typescript day 3");
+
+
+// ? Primitive and non primitive
+
+// types in typescript
+
+// 1. ? built in types  are the types which is provided by the typescript only
+// 2. ? user define in types --> are the types which will be created by the user .
+
+// ? builtin types
+
+// ? number
+// ? void
+// ? string
+// ? boolean
+// ? null
+
+// let age; // type inference is working when you initialize your variable at the time of declaration.
+
+
+// age = 25;
+
+// age = "78";
+
+
+// let age: number = 45;
+
+// ? string
+
+// let name1: string = "imran";
+// console.log(name1);
+
+// boolean
+
+// let isMarried: boolean = true;
+// console.log(isMarried);
+
+// null
+
+// let bankBalance: null=null;
+// console.log(typeof typeof bankBalance);
+
+
+
+// console.log("this is day3 ");
+
+
+
+// ? function
+
+//? it is a block of code which will perform some specific task for the end user , to utilise the function we need to call it .
+
+
+function add(a: number, b: number){
+    console.log(a+b);
+    
+    
+  return "hii"
+    
+}
+
+console.log(add(10, 20));
+
+
+
+// console.log(add(10, 50));
+// console.log(add(10, 60));
+
+
+
+
+
+
+
+
+
+
+
+
