0b326545791b7d43e5d4a3237737f0bde7958003
atul gathwala <atulgathwala1728@gmail.com>
2025-02-28 19:34:37 +0530

    Day 9 typescript

---
+++ Day8/Index.ts
@@ -0,0 +1,107 @@
+// console.log("This is Day8 typescript");
+
+// ? default assignability
+
+// let a: any = 45;
+
+// a.toLowerCase();
+
+// let b: unknown = 45;
+// b.toLwerCase();
+
+// let a: any = 45;
+// let b: unknown = 45;
+
+// let c: string = a;
+
+// console.log(typeof c);
+
+// let d:any = b;
+
+
+// ? Union
+
+// let isMarried: boolean | string | number = 45;
+
+// type a = {
+//     a:string
+// }
+// type b = {
+//     b:number
+// }
+// type c = {
+//     c:boolean
+// }
+
+// // ? union
+
+// // let obj: a|b|c = {
+// // c:true
+// // }
+
+// // ? intersection
+
+
+
+// let obj: a&b&c = {
+//     a: "hii",
+//     b: 45,
+//     c:true
+// }
+
+
+// ? enums in Typescript
+// ? enum is the short form of enumeration , this is the way to define a set of named values  , these values be used to represents a collection of related constants in a more organized manner
+
+// let weekDaysObj = {
+//     "0": "sunday",
+//     "1":"tuesday"
+// }
+
+// console.log(weekDaysObj);
+
+
+// ?In typescript we have two types of enums
+// number enum
+// string enum
+
+// let obj = {
+//     name: "atul",
+//     age: 45
+// }
+
+// console.log(obj["atul"]);
+
+
+
+// enum WeekDays{
+//     sunday=5,
+//     monday=7,
+//     tuesday,
+//     wednesday=8,
+//     thursday,
+//     friday,
+//     saturday
+
+// }
+
+// console.log(WeekDays.monday);
+
+
+
+enum TrafficLight {
+    yellow,
+    red ="RED",
+    green ,
+}
+
+
+// let signalInfo = TrafficLight.red;
+
+
+// console.log(signalInfo);
+
+
+
+
+
---
+++ Day9/index.ts
@@ -0,0 +1,95 @@
+// console.log("This is day 9 Typescript");
+
+
+// // ? interface
+
+// // ? an interface is used to define the structure of an object , whatever the properties you are going to define you mandatorily needs to utilze . in the interface we can only provide the declaration we are not supposed to provided the values to the declaration.
+
+
+// // interface EMP {
+// //     name: string;
+// //     greet():void;
+// // }
+
+
+// // let obj: EMP = {
+// //     name: "kavitha",
+// //     greet() {
+// //         console.log("This is greet from obj");
+        
+// //     }
+    
+// // }
+
+// // obj.greet();
+
+// // ? Checking the operations on interface
+
+// // interface name{
+// //     name:string
+
+// // }
+
+// // interface age{
+// //     age:number
+// // }
+
+
+
+// // let Person: name & age = {
+// //     name: "imran",
+// //     age:45,
+// // }
+
+
+
+// // console.log(Person);
+
+// // let Person2: name | age={
+// //     name:"imran"
+// // }
+
+
+// interface EMP {
+//     name: string,
+//     age: number,
+//    readonly email:string
+//     isMarried ?:boolean
+// }
+
+
+// let person1: EMP = {
+//     name: "sneha",
+//     email:"abc@gmail.com",
+//     age: 99,
+// }
+
+// console.log(person1.name);
+// person1.name = "neha"
+
+// console.log(person1.name);
+// // person1.email = "BCJAHB"
+// console.log(person1.email);
+
+
+
+
+// ? union 
+
+
+
+let isMarried: string | boolean | number | [] | {} = "yess";
+
+type A={
+a:number
+}
+
+type B= {
+    b:string
+}
+
+let obj: A & B = {
+    b: "amit",
+    a:456
+       
+}
